5479f28fccec9c13fc42c87b4ec3ba47431996b8
AnugrahSA <100453661+AnugrahSA@users.noreply.github.com>
2023-04-14 04:59:18 +0700

    Mengganti tipe data primitive
    Mengganti tipe data primitive dari gender dan mengganti tipe data yearJoined, monthJoined, dayJoined

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Date;
 
 public class Employee {
 
@@ -13,13 +14,15 @@ public class Employee {
 	private String idNumber;
 	private String address;
 	
-	private int yearJoined;
-	private int monthJoined;
-	private int dayJoined;
+	private Date tglkerja; 
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
+	private enum jenisgender{
+			Pria,
+			Wanita
+	}
+	private jenisgender gender;
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -31,15 +34,13 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address,Date tglkerja, boolean isForeigner, jenisgender gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
 		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
+		this.tglkerja = tglkerja;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
 		
@@ -94,8 +95,8 @@ public class Employee {
 		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
 		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
+		if (date.getYear() == tglkerja) {
+			monthWorkingInYear = date.getMonthValue() - tglkerja;
 		}else {
 			monthWorkingInYear = 12;
 		}
